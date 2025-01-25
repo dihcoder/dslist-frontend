ec0528b0b8d48e021db9a97c5eba979eaaa05528
dihcoder <mdsidiego@gmail.com>
2025-01-25 01:15:48 -0300

    Create Collection and Game pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { GameComponent } from './pages/game/game.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'collections',
+        component: CollectionsComponent
+    },
+    {
+        path: 'game',
+        component: GameComponent
     }
 ];
