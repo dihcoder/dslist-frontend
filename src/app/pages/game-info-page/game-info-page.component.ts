c6e9f93186bb0537463f5dbe8f01aca05ee2887f
dihcoder <mdsidiego@gmail.com>
2025-01-30 22:39:23 -0300

    feat: Game info page created

---
+++ src/app/pages/game-info-page/game-info-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-info-page',
+  imports: [],
+  templateUrl: './game-info-page.component.html',
+  styleUrl: './game-info-page.component.scss'
+})
+export class GameInfoPageComponent {
+
+}
