319ba3ff3dd87a0a22ef74fe066973804fc36be5
dihcoder <mdsidiego@gmail.com>
2025-01-30 21:31:23 -0300

    Push remaining files

---
+++ src/app/pages/game-list-page/game-list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-list-page',
+  imports: [],
+  templateUrl: './game-list-page.component.html',
+  styleUrl: './game-list-page.component.scss'
+})
+export class GameListPageComponent {
+
+}
